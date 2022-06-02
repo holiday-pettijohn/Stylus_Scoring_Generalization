1741942d1e0d31ecb3db19fb2e403bec621fe984
holiday-pettijohn <holiday.pettijohn@biola.edu>
2022-06-02 14:17:41 -0700

    Added stroke by stroke character rendering

---
+++ load_strokes.py
@@ -0,0 +1,48 @@
+import os
+
+import numpy as np
+import tensorflow as tf
+
+from PIL import Image
+from sklearn.model_selection import train_test_split
+
+def loadXy(data_dir, han_char):
+    dir_list = os.listdir(f"{data_dir}/{han_char}")
+    X_data = []
+    y_data = []
+    max_strokes = 0
+    for f in dir_list:
+        yX = open(f"{data_dir}/{han_char}/{f}", "rb").readlines()
+        y_data.append(float(yX[0]))
+        X_data.append(np.array([np.frombuffer(X, dtype=bool)[:-1].reshape(32, 32).astype(int) for X in yX[1:]]))
+        if X_data[-1].shape[0] > max_strokes:
+            max_strokes = X_data[-1].shape[0]
+    for i in range(len(X_data)):
+        while X_data[i].shape[0] < max_strokes:
+            X_data[i] = np.append(X_data[i], np.zeros((1, 32, 32)), 0)
+    X_data = np.array(X_data)
+    y_data = np.array(y_data)
+    return X_data, y_data
+
+def loadXyCharSplits(data_dir, han_char, test_size = 0.2, random_state = 42):
+    X_data, y_data = loadXy(data_dir, han_char)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_data, y_data, test_size=test_size, random_state=random_state
+    )
+    X_train, X_test, y_train, y_test = (tf.convert_to_tensor(X_train, tf.float16),
+                                        tf.convert_to_tensor(X_test, tf.float16),
+                                        tf.convert_to_tensor(y_train, tf.float16),
+                                        tf.convert_to_tensor(y_test, tf.float16),
+                                       )
+    return X_train, X_test, y_train, y_test
+
+def displayBinary(arr):
+    img = Image.fromarray(arr)
+    return img
+
+def displayBinaryStrokes(strokes):
+    arr = strokes[0]
+    for a in strokes[1:]:
+        arr = np.logical_or(arr, a)
+    img = Image.fromarray(arr)
+    return img
--- xmlparse.py
+++ xmlparse.py
@@ -58,90 +58,6 @@ def scanDir(xdir, out_dir="./HanBitmap", recursive=False, verbose=0, from_recurs
             fy.write(files_y[han_char])
     else:
         return files_X, files_y
-    
-def dupeCheck(out_dir="./HanBitmap", files=()):
-    """
-    Checks the given files in the output directory for duplicates
-    If no files are specified, all files in the directory, both X and y, will be checked
-    """
-    if len(files) == 0:
-        files = os.listdir(out_dir)
-    for f in files:
-        if f.endswith("_X"):
-            han_char = f.split("_")[0]
-            # check for duplicates
-            fX = open(f"{out_dir}/{han_char}_X", "rb")
-            fX_new = np.array(fX.readlines())
-            uniques, u_filter = np.unique(fX_new, return_index=True, axis=0)
-            if len(fX_new) == len(uniques):
-                fX.close()
-            else:
-                fX_new = fX_new[u_filter]
-                fX_str = b""
-                for s in fX_new:
-                    fX_str += s
-                fX.close()
-                fX = open(f"{out_dir}/{han_char}_X", "wb")
-                fX.write(fX_str)
-                fX.close()
-                # only open fy if we made changes to fX
-                fy = open(f"{out_dir}/{han_char}_y", "r")
-                fy_new = np.array(fy.readlines())[u_filter]
-                fy_str = ""
-                for s in fy_new:
-                    fy_str += s
-                fy.close()
-                fy = open(f"{out_dir}/{han_char}_y", "w")
-                fy.write(fy_str)
-                fy.close()
-                
-def dupeCheckNew(han_char, new_X, new_y, out_dir="./HanBitmap"):
-    """
-    Checks new data for duplicates and removes them while appending the new data to an existing file
-    Seems to have some issues when presented with a large dataset
-    """
-    files = os.listdir(out_dir)
-    if f"{han_char}_X" not in files:
-        fX = open(f"{out_dir}/{han_char}_X", "wb")
-        fX.write(new_X)
-        fy = open(f"{out_dir}/{han_char}_y", "w")
-        fy.write(new_y)
-        fX.close()
-        fy.close()
-        return
-    # check for duplicates
-    fX = open(f"{out_dir}/{han_char}_X", "rb")
-    fX_old = fX.readlines()
-    fX_new = np.concatenate((np.array(fX_old), np.array(new_X.splitlines())))
-    uniques, u_filter = np.unique(fX_new, return_index=True, axis=0)
-    if len(fX_new) == len(uniques):
-        fX = open(f"{out_dir}/{han_char}_X", "ab")
-        fX.write(new_X)
-        fy = open(f"{out_dir}/{han_char}_y", "a")
-        fy.write(new_y)
-        fX.close()
-        fy.close()
-    elif len(fX_old) == len(uniques):
-        pass
-    else:
-        fX_new = fX_new[u_filter]
-        fX_str = b""
-        for s in fX_new:
-            fX_str += s
-        fX.close()
-        fX = open(f"{out_dir}/{han_char}_X", "wb")
-        fX.write(fX_str)
-        fX.close()
-        # only open fy if we made changes to fX
-        fy = open(f"{out_dir}/{han_char}_y", "r")
-        fy_new = np.concatenate((np.array(fy.readlines()), np.array(new_y.splitlines())))[u_filter]
-        fy_str = ""
-        for s in fy_new:
-            fy_str += s
-        fy = open(f"{out_dir}/{han_char}_y", "w")
-        fy.write(fy_str)
-        fX.close()
-        fy.close()
 
 def xmlToBitmap(xfile):
     """
@@ -194,14 +110,112 @@ def drawXml(segments, output_size=(32, 32), border=4):
                 draw.line(((x1, y1), (x2, y2)), width=1, fill=1)
     return np.array(img).flatten().tobytes()
 
+def xmlToSegments(xfile):
+    """
+    Convert an XML character genome file to a bitmap array representing the character
+    Saves the fitness score of said genome along with the bitmap
+    """
+    xml_data = open(xfile, "r").read()
+    root = xmltodict.parse(xml_data)
+    score = root["genome"]["statistics"]["@score"]
+    han_char = root["genome"]["genes"]["gene"]["hanReferences"]["hanReference"]["@unicode"]
+    segments = root["genome"]["genes"]["gene"]["segments"]["segment"]
+    drawn_char = drawSegments(segments)
+    return (han_char, score, drawn_char)
+
+def drawSegments(segments, output_size=(32, 32), border=4):
+    """
+    Process and render the coherent strokes in the xml data
+    Saves the segments as line seperated byte arrays
+    """
+    imgs = b""
+    minx, miny, maxx, maxy = None, None, None, None
+    for segment in segments:
+        if segment["@coherent"] == "true":
+            for point in segment["point"]:
+                if minx is None:
+                    minx = float(point["@x"])
+                if miny is None:
+                    miny = float(point["@y"])
+                if maxx is None:
+                    maxx = float(point["@x"])
+                if maxy is None:
+                    maxy = float(point["@y"])
+                if float(point["@x"]) > maxx:
+                    maxx = float(point["@x"])
+                if float(point["@x"]) < minx:
+                    minx = float(point["@x"])
+                if float(point["@y"]) > maxy:
+                    maxy = float(point["@y"])
+                if float(point["@y"]) < miny:
+                    miny = float(point["@y"])
+    for segment in segments:
+        if segment["@coherent"] == "true":
+            img = Image.new(mode="1", size=output_size)
+            draw = ImageDraw.Draw(img)
+            for i in range(len(segment["point"][:-1])):
+                x1, y1, x2, y2 = ((float(segment["point"][i]["@x"])-minx)*((output_size[0]-border)/(maxx-minx))+border/2,
+                                  (float(segment["point"][i]["@y"])-miny)*((output_size[1]-border)/(maxy-miny))+border/2,
+                                  (float(segment["point"][i+1]["@x"])-minx)*((output_size[0]-border)/(maxx-minx))+border/2,
+                                  (float(segment["point"][i+1]["@y"])-miny)*((output_size[1]-border)/(maxy-miny))+border/2)
+                y1 = -(y1-output_size[1]/2)+output_size[1]/2
+                y2 = -(y2-output_size[1]/2)+output_size[1]/2
+                draw.line(((x1, y1), (x2, y2)), width=1, fill=1)
+            imgs += np.array(img).flatten().tobytes() + b"\n"
+    return imgs
+
+def scanSegments(xdir, out_dir="./HanBitmap", recursive=False, verbose=0, from_recursive=False, han_i={}):
+    """
+    Iterates over a directory and scans all of the gene files within, outputting them in the form of bitmap (X) and score (y)
+    """
+    if (int(verbose) >= 1 and not from_recursive) or (int(verbose) >= 2):
+        print(f"Scanning directiory {xdir}...")
+    dir_list = os.listdir(xdir)
+    han_i_new = {}
+    for f in dir_list:
+        if not f.startswith("."):
+            if path.isdir(os.path.join(xdir, f)) and recursive:
+                if int(verbose) >= 1:
+                    print(f"Recursively scanning child directory {f}...")
+                han_i = scanSegments(path.join(xdir, f), out_dir=out_dir, recursive=recursive, verbose=verbose, from_recursive=True, han_i=han_i)
+            elif f.endswith(".gene"):
+                if int(verbose) >= 2:
+                    print(f"Scanning gene file {f}...")
+                han_char, score, bitmap = xmlToSegments(f"{xdir}/{f}")
+                if not os.path.isdir(f"{out_dir}/{han_char}"):
+                    os.mkdir(f"{out_dir}/{han_char}")
+                if not han_char in han_i:
+                    han_i[han_char] = 0
+                if not han_char in han_i_new:
+                    han_i_new[han_char] = 0
+                dfile = open(f"{out_dir}/{han_char}/{han_i[han_char]}", "wb")
+                dfile.write(bytes(score, "UTF-8")+b"\n"+bitmap)
+                han_i[han_char] += 1
+                han_i_new[han_char] += 1
+    if int(verbose) >= 1:
+        total_files = 0
+        for han_char in han_i_new:
+            total_files += han_i_new[han_char]
+        print(f"Processed {total_files} files in directory {xdir}")
+        if not from_recursive:
+            final_total_files = 0
+            for han_char in han_i:
+                final_total_files += han_i[han_char]
+            print(f"Processed a total of {final_total_files} files")
+    return han_i
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Parse the XML gene files in a given directory and output the binary results in the output directory")
     parser.add_argument("--in_dir", nargs="?", const=1, default="Genes", help="The directory with the XML files you want to scan")
     parser.add_argument("--out_dir", nargs="?", const=1, default="HanBitmap", help="The directory where you want to output the binary data")
     parser.add_argument("--recursive", "-r", action=argparse.BooleanOptionalAction, help="Whether to recursively search through the input directory for files")
     parser.add_argument("--verbose", "-v", nargs="?", const=1, default=0, help="Whether to print additional information as the program runs")
+    parser.add_argument("--whole", "-w", default=False, action=argparse.BooleanOptionalAction, help="Whether to render whole characters instead of segments")
     args = parser.parse_args()
     print(f"Processing gene directory {args.in_dir}, writing results to {args.out_dir}")
-    scanDir(args.in_dir, args.out_dir, recursive=args.recursive, verbose=args.verbose)
+    if args.whole:
+        scanDir(args.in_dir, args.out_dir, recursive=args.recursive, verbose=int(args.verbose))
+    else:
+        scanSegments(args.in_dir, args.out_dir, recursive=args.recursive, verbose=int(args.verbose))
     print("Done")
 
